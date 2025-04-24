4e560686f42559ffc4f38e26afdc756420696fa1
JuanMaDev <juanmarcoso@gmail.com>
2025-04-24 09:18:27 -0300

    Django - messages success
    Making messages when update your profile

--- Bookmarks/account/forms.py
+++ Bookmarks/account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -20,3 +21,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords does not match.')
         return cd['password2']
+    
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        # We're going to use only this fields
+        fields = ['first_name', 'last_name', 'email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        # We're going to use this fields
+        fields = ['date_of_birth', 'photo']
--- Bookmarks/account/urls.py
+++ Bookmarks/account/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 
 ]
